44a1a3ba3441db6679d427344df08dc572742a72
Beratdmrbs <beratdmrbs61@gmail.com>
2024-11-29 21:06:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bir cümle giriniz:");
+        string cumle = Console.ReadLine();
+        string[] kelimeler = cumle.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        int kelimeSayisi = kelimeler.Length;
+        Console.WriteLine("Cümledeki kelime sayısı: " + kelimeSayisi);
+    }
+}
+
